7cd654c46a8c89aa2277430144bf61e15b02bbea
arsen <arsen.dmitrenko1994@gmail.com>
2022-09-24 15:51:28 +0300

    feat: add graphql dependencies

--- apps/backend/api/src/environments/environment.ts
+++ apps/backend/api/src/environments/environment.ts
@@ -1,12 +1,36 @@
+// export const environment = {
+//   production: false,
+//   connection: {
+//     type: process.env.DB_TYPE as 'aurora-mysql',
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     username: process.env.DB_USER_NAME,
+//     password: process.env.DB_USER_PASSWORD,
+//     database: process.env.DB_NAME,
+//     dropSchema: false,
+//     synchronize: true,
+//     logging: true,
+//     entities: [
+//       'dist/apps/backend/api/src/**/*.entity{.ts, .js}'
+//     ],
+//     migrations: [
+//       'dist/apps/backend/api/migrations/*{.ts, .js}'
+//     ],
+//     cli: {
+//       'migrationsDir': 'db/migrations'
+//     }
+//   }
+// };
+
 export const environment = {
   production: false,
   connection: {
     type: 'postgres' as 'aurora-mysql',
-    host: process.env.DB_HOST,
+    host: 'localhost',
     port: Number(process.env.DB_PORT),
     username: 'postgres',
-    password: '1234',
-    database: process.env.DB_NAME,
+    password: '1994',
+    database: 'postgres',
     dropSchema: false,
     synchronize: true,
     logging: true,
